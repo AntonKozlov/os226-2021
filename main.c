8e6c2063868d14478bfddf2f376ccad8dae11344
z <loktevsergay.2003@gmail.com>
2021-09-10 10:56:37 +0300

    Second implem

--- main.c
+++ main.c
@@ -31,20 +31,23 @@ int cmd_executor(char **words, int words_len) {
     }
 }
 
-void cmd_parser() {
-    char buf[STRING_PIECE];
+int cmd_parser() {
+    char buf[STRING_PIECE] = "";
     while (1) {
         char *line;
-        if (strlen(buf) > 0) {
-            line = malloc(strlen(buf));
-            strcpy(line, buf);
+        unsigned long buf_len = strlen(buf);
+        if (buf_len > 0) {
+            line = malloc(buf_len);
+            memcpy(line, buf, buf_len);
             strcpy(buf, "");
         } else {
             line = NULL;
+            unsigned long line_len = 0;
             do {
-                if (fgets(buf, STRING_PIECE, stdin) == NULL) break;
+                if (fgets(buf, STRING_PIECE, stdin) == NULL)  return 0;
                 line = realloc(line, sizeof(line) + strlen(buf));
-                strcat(line, buf);
+                strcpy(line + line_len, buf);
+                line_len += strlen(buf);
             } while (strlen(buf) == STRING_PIECE - 1 && buf[STRING_PIECE - 1] != '\n');
         }
         strcpy(buf, "");
@@ -64,15 +67,17 @@ void cmd_parser() {
         char *word = strtok(next_cmd, " ");
         if (word == NULL) word = next_cmd;
         char **words = NULL;
-        int words_len = 0;
+        int words_len = 1;
         while (word != NULL) {
-            words = realloc(words, sizeof(words) + sizeof(char *));
-            words[words_len] = word;
+            words = realloc(words, words_len * sizeof(char *));
+            words[words_len - 1] = word;
             words_len++;
             word = strtok(NULL, " ;");
         }
+        words_len--;
         return_code = cmd_executor(words, words_len);
-        free(line);
+        free(next_cmd);
+        free(words);
     }
 }
 
