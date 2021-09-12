ef537569ada7f12c724416379479e1c8c0dc5f36
Slava <st091713@student.spbu.ru>
2021-09-12 18:23:15 +0300

    Сommand handler has been written

--- main.c
+++ main.c
@@ -1,17 +1,66 @@
-
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_COMMANDS 256
+#define MAX_LENGTH 256
+#define ECHO_LENGTH 4
+#define RETCODE_LENGTH 7
+#define COMMAND_DELIM ";\n"
+#define WORD_DELIM " \n"
 
 int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
-		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
-	}
-	return argc - 1;
+    for (int i = 1; i < argc; ++i) {
+        printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
+    }
+    return argc - 1;
 }
 
 int retcode(int argc, char *argv[]) {
+    printf("%s\n", argv[1]);
+    return 0;
+}
+
+int cmd() {
+    int return_code = 0;
+    while(1) {
+        char string[MAX_LENGTH] = "";
+        if (fgets(string, MAX_LENGTH, stdin) == NULL) {
+            break;
+        }
+        char * commands[MAX_COMMANDS];
+        char * command = strtok(string, COMMAND_DELIM);
+        int count_of_commands = 0;
+        while(command != NULL) {
+            commands[count_of_commands++] = command;
+            command = strtok(NULL, COMMAND_DELIM);
+        }
+        for (int j = 0; j < count_of_commands; j++) {
+            char * words[MAX_COMMANDS];
+            char * word = strtok(commands[j], WORD_DELIM);
+            int count_of_words = 0;
+            while (word != NULL) {
+                words[count_of_words++] = word;
+                word = strtok(NULL, WORD_DELIM);
+            }
+            if (strncmp(words[0], "echo", ECHO_LENGTH) == 0) {
+                return_code = echo(count_of_words, words);
+            } else if (strncmp(words[0], "retcode", RETCODE_LENGTH) == 0) {
+                char * argv[128];
+                char buffer[MAX_LENGTH];
+                sprintf(buffer, "%d", return_code);
+                argv[0] = words[0];
+                argv[1] = buffer;
+                return_code = retcode(2, argv);
+            } else {
+                return_code = -1;
+                printf("Unknown command '%s'\n", words[0]);
+            }
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-	// IMPL ME
-	return 0;
+    cmd();
+    return 0;
 }
