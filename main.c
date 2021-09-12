acb42ef570188948ec2c0114d3bd11104bd54a49
AShlegel <st087000@student.spbu.ru>
2021-09-12 19:40:33 +0300

    Fix code style

--- main.c
+++ main.c
@@ -3,71 +3,96 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_INPUT_STRING_LENGTH 1023
+#define MAX_INPUT_STRING_LENGTH 1024
 #define COMMAND_DELIM ";\n"
 #define TOKEN_DELIM " "
 
-int code = 0;
+enum {
+    OK,
+    ERROR = -1
+};
+
+int return_code = OK;
+
+//------------------------------------------------------------------------------------------------------------- COMMANDS
+
+#define ECHO "echo"
+#define RETCODE "retcode"
+
+//--------------------------------------------------------------------------------------------------------- DECLARATIONS
+
+int echo(int argc, char* argv[]);
+
+int ret_code(int argc, char* argv[]);
+
+int exec_commands(char** tokens, int tnk_num);
+
+int parse_with_delim(char* buf, char*** commands, int* cmd_num, const char* delim);
+
+//----------------------------------------------------------------------------------------------------------------------
 
 int echo(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
-        if (printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ') < 0) return -1;
+        if (printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ') < 0)
+            return ERROR;
     }
     return argc - 1;
 }
 
 int ret_code(int argc, char* argv[]) {
-    if (printf("%d\n", code) >= 0) return 0;
-    else return -1;
+    if (printf("%d\n", return_code) < 0)
+        return ERROR;
+    else return OK;
 }
 
-int parse_tokens(char* cmd, char*** tokens, int* n_t) {
-    *tokens = NULL;
-    *n_t = 0;
-    char* tkn = strtok(cmd, TOKEN_DELIM);
-    while (tkn) {
-        *tokens = realloc(*tokens, sizeof(char*) * ++(*n_t));
-        (*tokens)[(*n_t) - 1] = tkn;
-        tkn = strtok(NULL, TOKEN_DELIM);
-    }
-    return 0;
-}
-
-int exec_commands(char** tokens, const int n_t) {
+int exec_commands(char** tokens, const int tnk_num) {
     char* cmd_name = tokens[0];
-    if (strcmp(cmd_name, "echo") == 0)
-        code = echo(n_t, tokens);
-    else if (strcmp(cmd_name, "retcode") == 0)
-        code = ret_code(n_t, tokens);
-    return 0;
+    if (strcmp(cmd_name, ECHO) == 0)
+        return_code = echo(tnk_num, tokens);
+    else if (strcmp(cmd_name, RETCODE) == 0)
+        return_code = ret_code(tnk_num, tokens);
+    else if (printf("%s: command not found\n", cmd_name) < 0)
+        return ERROR;
+    return OK;
 }
 
-int parse_commands(char* buf, char*** commands, int* n_c) {
+int parse_with_delim(char* buf, char*** commands, int* cmd_num, const char* delim) {
     *commands = NULL;
-    *n_c = 0;
-    char* cmd = strtok(buf, COMMAND_DELIM);
+    *cmd_num = 0;
+    char* cmd = strtok(buf, delim);
     while (cmd) {
-        *commands = realloc(*commands, sizeof(char*) * ++(*n_c));
-        (*commands)[(*n_c) - 1] = cmd;
-        cmd = strtok(NULL, COMMAND_DELIM);
+        *commands = realloc(*commands, sizeof(char*) * ++(*cmd_num));
+        if (!*commands)
+            return ERROR;
+        (*commands)[(*cmd_num) - 1] = cmd;
+        cmd = strtok(NULL, delim);
     }
-    return 0;
+    return OK;
 }
+
+//----------------------------------------------------------------------------------------------------------------- MAIN
 
 int main(int argc, char* argv[]) {
     char buf[MAX_INPUT_STRING_LENGTH];
-    char** commands;
-    int n_c;
     while (fgets(buf, MAX_INPUT_STRING_LENGTH, stdin) != NULL) {
-        parse_commands(buf, &commands, &n_c);
-
-        for (int i = 0; i < n_c; i++) {
-            char** tokens;
-            int n_t;
-            parse_tokens(commands[i], &tokens, &n_t);
-            exec_commands(tokens, n_t);
+        char** commands;
+        int cmd_num;
+        if (parse_with_delim(buf, &commands, &cmd_num, COMMAND_DELIM) != 0) {
+            free(commands);
+            return ERROR;
         }
+        for (int i = 0; i < cmd_num; i++) {
+            char** tokens;
+            int tnk_num;
+            if (parse_with_delim(commands[i], &tokens, &tnk_num, TOKEN_DELIM) != 0 ||
+                exec_commands(tokens, tnk_num) != 0) {
+                free(tokens);
+                free(commands);
+                return ERROR;
+            }
+            free(tokens);
+        }
+        free(commands);
     }
-    return 0;
+    return OK;
 }
-
