1e3fbc5c279cb18e9373050ba2e3b89109451448
TimPushkin <pushkin.td@gmail.com>
2021-09-18 17:56:49 +0300

    Implement obstack

--- pool.c
+++ pool.c
@@ -1,14 +1,30 @@
-
 #include <stddef.h>
+#include <stdio.h>
 
 #include "pool.h"
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz) {
+    *p = (struct pool) POOL_INITIALIZER(mem, nmemb, membsz);
 }
 
-void *pool_alloc(struct pool *p) {
-	return NULL;
+void* pool_alloc(struct pool* p) {
+    if (p->next == p->end) {
+        fprintf(stderr, "Allocation failed: pool %p is full\n", p->base);
+        return NULL;
+    }
+
+    void* r = p->next;
+    p->next += p->size;
+    return r;
 }
 
-void pool_free(struct pool *p, void *ptr) {
+void pool_free(struct pool* p, void* ptr) {
+    if (ptr < p->base || ptr >= p->next || (ptr - p->base) % p->size != 0) {
+        fprintf(stderr, "Freeing failed: %p is not from pool %p\n", ptr, p->base);
+        return;
+    }
+
+    while (p->next != ptr) {
+        p->next -= p->size;
+    }
 }
--- pool.h
+++ pool.h
@@ -3,17 +3,18 @@
 #include "util.h"
 
 struct pool {
-
+    void* base;
+    void* next;
+    size_t size;
+    void* end;
 };
 
-#define POOL_INITIALIZER(_mem, _nmemb, _membsz) { \
-}
+#define POOL_INITIALIZER(_mem, _nmemb, _membsz) { _mem, _mem, _membsz, ((void*) (_mem)) + (_nmemb) * (_membsz) }
 
-#define POOL_INITIALIZER_ARRAY(_array) \
-	POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
+#define POOL_INITIALIZER_ARRAY(_array) POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]))
 
-void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz);
+void pool_init(struct pool* p, void* mem, unsigned long nmemb, unsigned long membsz);
 
-void *pool_alloc(struct pool *p);
+void* pool_alloc(struct pool* p);
 
-void pool_free(struct pool *p, void *ptr);
+void pool_free(struct pool* p, void* ptr);
