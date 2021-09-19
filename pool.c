04aa85671314538c17a4ac406043065cedccb6c3
ivan <azarnikov01@gmail.com>
2021-09-19 15:28:11 +0300

    First commit

--- pool.c
+++ pool.c
@@ -1,14 +1,35 @@
-
 #include <stddef.h>
 
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+    *p = POOL_INITIALIZER(mem, nmemb, membsz);
 }
 
 void *pool_alloc(struct pool *p) {
+    struct pool_free_block *fb = p->free;
+    if (fb) {
+        p->free = fb->next;
+        return fb;
+    }
+    if (p->freestart < p->freeend) {
+        void *r = p->freestart;
+        p->freestart += p->membsz;
+        return r;
+    }
 	return NULL;
 }
 
+int belong_pool(struct pool *p, void *ptr) {
+    return p->mem <= ptr && p->freeend >= ptr;
+}
+
 void pool_free(struct pool *p, void *ptr) {
+    if (belong_pool(p, ptr)) {
+        struct pool_free_block *fb = ptr;
+        *fb = (struct pool_free_block) {
+            .next = p->free
+        };
+        p->free = fb;
+    }
 }
