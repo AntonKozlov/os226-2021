9695a01b72fd469030f6fe6b1882b593cecd3d6c
IraSheremet <irasheremet2002@gmail.com>
2021-09-19 22:32:22 +0300

    Pool implementation

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,12 +14,15 @@ static int g_retcode;
 
 
 #define DECLARE(X) static int X(int, char *[]);
+
 APPS_X(DECLARE)
+
 #undef DECLARE
 
 static const struct app {
-        const char *name;
-        int (*fn)(int, char *[]);
+    const char *name;
+
+    int (*fn)(int, char *[]);
 } app_list[] = {
 #define ELEM(X) { # X, X },
         APPS_X(ELEM)
@@ -28,86 +30,86 @@ static const struct app {
 };
 
 static int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
-		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
-	}
-	return argc - 1;
+    for (int i = 1; i < argc; ++i) {
+        printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
+    }
+    return argc - 1;
 }
 
 static int retcode(int argc, char *argv[]) {
-	printf("%d\n", g_retcode);
-	return 0;
+    printf("%d\n", g_retcode);
+    return 0;
 }
 
 static int exec(int argc, char *argv[]) {
-	const struct app *app = NULL;
-	for (int i = 0; i < ARRAY_SIZE(app_list); ++i) {
-		if (!strcmp(argv[0], app_list[i].name)) {
-			app = &app_list[i];
-			break;
-		}
-	}
+    const struct app *app = NULL;
+    for (int i = 0; i < ARRAY_SIZE(app_list); ++i) {
+        if (!strcmp(argv[0], app_list[i].name)) {
+            app = &app_list[i];
+            break;
+        }
+    }
 
-	if (!app) {
-		printf("Unknown command\n");
-		return 1;
-	}
+    if (!app) {
+        printf("Unknown command\n");
+        return 1;
+    }
 
-	g_retcode = app->fn(argc, argv);
-	return g_retcode;
+    g_retcode = app->fn(argc, argv);
+    return g_retcode;
 }
 
 static int pooltest(int argc, char *argv[]) {
-	struct obj {
-		void *field1;
-		void *field2;
-	};
-	static struct obj objmem[4];
-	static struct pool objpool = POOL_INITIALIZER_ARRAY(objmem);
+    struct obj {
+        void *field1;
+        void *field2;
+    };
+    static struct obj objmem[4];
+    static struct pool objpool = POOL_INITIALIZER_ARRAY(objmem);
 
-	if (!strcmp(argv[1], "alloc")) {
-		struct obj *o = pool_alloc(&objpool);
-		printf("alloc %d\n", o ? (o - objmem) : -1);
-		return 0;
-	} else if (!strcmp(argv[1], "free")) {
-		int iobj = atoi(argv[2]);
-		printf("free %d\n", iobj);
-		pool_free(&objpool, objmem + iobj);
-		return 0;
-	}
+    if (!strcmp(argv[1], "alloc")) {
+        struct obj *o = pool_alloc(&objpool);
+        printf("alloc %ld\n", o ? (o - objmem) : -1);
+        return 0;
+    } else if (!strcmp(argv[1], "free")) {
+        int iobj = atoi(argv[2]);
+        printf("free %d\n", iobj);
+        pool_free(&objpool, objmem + iobj);
+        return 0;
+    }
 }
 
 int shell(int argc, char *argv[]) {
-	char line[256];
-	while (fgets(line, sizeof(line), stdin)) {
-		const char *comsep = "\n;";
-		char *stcmd;
-		char *cmd = strtok_r(line, comsep, &stcmd);
-		while (cmd) {
-			const char *argsep = " ";
-			char *starg;
-			char *arg = strtok_r(cmd, argsep, &starg);
-			char *argv[256];
-			int argc = 0;
-			while (arg) {
-				argv[argc++] = arg;
-				arg = strtok_r(NULL, argsep, &starg);
-			}
-			argv[argc] = NULL;
+    char line[256];
+    while (fgets(line, sizeof(line), stdin)) {
+        const char *comsep = "\n;";
+        char *stcmd;
+        char *cmd = strtok_r(line, comsep, &stcmd);
+        while (cmd) {
+            const char *argsep = " ";
+            char *starg;
+            char *arg = strtok_r(cmd, argsep, &starg);
+            char *argv[256];
+            int argc = 0;
+            while (arg) {
+                argv[argc++] = arg;
+                arg = strtok_r(NULL, argsep, &starg);
+            }
+            argv[argc] = NULL;
 
-			if (!argc) {
-				break;
-			}
+            if (!argc) {
+                break;
+            }
 
-			exec(argc, argv);
+            exec(argc, argv);
 
-			cmd = strtok_r(NULL, comsep, &stcmd);
-		}
-	}
-	return 0;
+            cmd = strtok_r(NULL, comsep, &stcmd);
+        }
+    }
+    return 0;
 }
 
 
 int main(int argc, char *argv[]) {
-	shell(0, NULL);
+    shell(0, NULL);
 }
--- pool.c
+++ pool.c
@@ -1,14 +1,29 @@
-
 #include <stddef.h>
 
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+    *p = POOL_INITIALIZER(mem, nmemb, membsz);
 }
 
 void *pool_alloc(struct pool *p) {
-	return NULL;
+    struct pool_free_block *fb = p->free;
+    if (fb) {
+        p->free = fb->next;
+        return fb;
+    }
+    if (p->freestart < p->freeend) {
+        void *r = p->freestart;
+        p->freestart += p->membsz;
+        return r;
+    }
+    return NULL;
 }
 
 void pool_free(struct pool *p, void *ptr) {
+    if (p->mem <= ptr && ptr <= p->freeend) {
+        struct pool_free_block *new_free_block = ptr;
+        new_free_block->next = p->free;
+        p->free = new_free_block;
+    }
 }
