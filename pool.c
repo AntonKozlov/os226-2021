3b5ceb1f5017003c0145bc4a8836f9b0fa88ea64
michael555444 <www.michael555444.by@gmail.com>
2021-09-22 02:50:57 +0300

    Test pool resolution

--- pool.c
+++ pool.c
@@ -4,11 +4,43 @@
 #include "pool.h"
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz) {
+
+    *p = POOL_INITIALIZER(mem, nmemb, membsz);
+
 }
 
 void *pool_alloc(struct pool *p) {
-	return NULL;
+
+    struct fb *fb = p -> f;
+
+    if (fb) {
+
+        p -> f = fb -> k;
+
+        return fb;
+
+    }
+
+    else if (p -> fs < p -> fe) {
+
+        void *r = p -> fs;
+
+        p -> fs += p -> mbsz;
+
+        return r;
+
+    }
+
+    return NULL;
+
 }
 
 void pool_free(struct pool *p, void *ptr) {
+
+    struct fb *fb = ptr;
+
+    *fb = (struct fb) {.k = p -> f};
+
+    p -> f = fb;
+
 }
--- pool.h
+++ pool.h
@@ -4,13 +4,27 @@
 
 struct pool {
 
+    long mbsz;
+
+    void *m;
+    void *fs;
+    void *fe;
+
+    struct fb { struct fb *k } *f;
+
 };
 
-#define POOL_INITIALIZER(_mem, _nmemb, _membsz) { \
+#define POOL_INITIALIZER(_mem, _nmemb, _membsz) (struct pool) { \
+                                                                \
+    .m = (_mem),                                                \
+    .f = NULL,                                                  \
+    .fs = (_mem),                                               \
+    .fe = ((void *)(_mem)) + (_nmemb) * (_membsz),              \
+    .mbsz = (_membsz)                                           \
+                                                                \
 }
 
-#define POOL_INITIALIZER_ARRAY(_array) \
-	POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
+#define POOL_INITIALIZER_ARRAY(_array) POOL_INITIALIZER(_array, ARRAY_SIZE(_array), sizeof((_array)[0]));
 
 void pool_init(struct pool *p, void *mem, unsigned long nmemb, unsigned long membsz);
 
