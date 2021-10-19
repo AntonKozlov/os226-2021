2f1dcd32743e16569ab0e05b037bf27d103ca634
Slava <st091713@student.spbu.ru>
2021-10-19 22:42:57 +0300

    HW5

--- timer.c
+++ timer.c
@@ -11,9 +11,28 @@
 #include "timer.h"
 
 int timer_cnt(void) {
-	// TODO: getitimer
+    struct itimerval timer;
+    getitimer(ITIMER_REAL, &timer);
+    int time_interval = timer.it_interval.tv_sec * 1000000 + timer.it_interval.tv_usec;
+    int time_value = timer.it_value.tv_sec * 1000000 + timer.it_value.tv_usec;
+
+    return (time_interval - time_value);
 }
 
 void timer_init(int ms, void (*hnd)(void)) {
-	// TODO: setitimer
+    struct sigaction sa;
+    struct itimerval timer;
+
+    memset(&sa, 0, sizeof(sa));
+
+    sa.sa_handler = hnd;
+
+    sigaction(SIGALRM, &sa, NULL);
+
+    timer.it_value.tv_sec = ms / 1000;
+    timer.it_value.tv_usec = (ms % 1000) * 1000;
+    timer.it_interval.tv_sec = ms / 1000;
+    timer.it_interval.tv_usec = (ms % 1000) * 1000;
+
+    setitimer(ITIMER_REAL, &timer, NULL);
 }
