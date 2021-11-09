c3761a6ed327d65d137aabb409521b438b42548e
Ilya Muravjov <muravjovilya@gmail.com>
2021-11-09 21:29:27 +0300

    Implement do_exec

--- sched.c
+++ sched.c
@@ -361,12 +361,10 @@ static int vmctx_brk(struct vmctx *vm, void *addr) {
 }
 
 int vmprotect(void *start, unsigned len, int prot) {
-#if 0
 	if (mprotect(start, len, prot)) {
 		perror("mprotect");
 		return -1;
 	}
-#endif
 	return 0;
 }
 
@@ -396,6 +394,45 @@ static int do_exec(const char *path, char *argv[]) {
 	// Find Elf64_Ehdr -- at the very start
 	//   Elf64_Phdr -- find one with PT_LOAD, load it for execution
 	//   Find entry point (e_entry)
+	Elf64_Ehdr eh;
+	read(fd, &eh, sizeof(Elf64_Ehdr));
+	lseek(fd, eh.e_phoff, SEEK_SET);
+	for (Elf64_Half i = 0; i < eh.e_phnum; i++) {
+		Elf64_Phdr ph;
+		read(fd, &ph, sizeof(Elf64_Phdr));
+		if (ph.p_type != PT_LOAD) continue;
+		vmctx_brk(&current->vm, (void *) (ph.p_vaddr + ph.p_memsz));
+		lseek(fd, ph.p_offset, SEEK_SET);
+		char page[PAGE_SIZE];
+		Elf64_Addr first_page = (ph.p_vaddr - (unsigned long) USER_START) / PAGE_SIZE;
+		Elf64_Xword page_count = ph.p_filesz / PAGE_SIZE;
+		for (Elf64_Xword j = 0; j < page_count; j++) {
+			read(fd, page, PAGE_SIZE);
+			lseek(memfd,PAGE_SIZE * current->vm.map[first_page + j], SEEK_SET);
+			write(memfd, page, PAGE_SIZE);
+		}
+		if (ph.p_filesz % PAGE_SIZE != 0) {
+			read(fd, page, ph.p_filesz % PAGE_SIZE);
+			lseek(memfd, PAGE_SIZE * current->vm.map[first_page + page_count], SEEK_SET);
+			write(memfd, page, ph.p_filesz % PAGE_SIZE);
+		}
+		lseek(memfd, 0, SEEK_SET);
+		struct ctx old, new;
+		vmctx_apply(&current->vm);
+		for (unsigned int j = 0; j < current->vm.brk; j++)
+			vmprotect(
+					USER_START,
+					PAGE_SIZE,
+					((ph.p_flags & PF_R) ? PROT_READ : 0) |
+					((ph.p_flags & PF_W) ? PROT_WRITE : 0) |
+					((ph.p_flags & PF_X) ? PROT_EXEC : 0)
+			);
+		current->main = (int (*)(int, char **)) eh.e_entry;
+		ctx_make(&new, exectramp, USER_START + USER_PAGES * PAGE_SIZE);
+		ctx_switch(&old, &new);
+		break;
+	}
+	return 0;
 }
 
 static void inittramp(void* arg) {
