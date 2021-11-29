27a03e200132460844839cefa40dabd6b82aa865
TimPushkin <pushkin.td@gmail.com>
2021-11-29 22:26:12 +0300

    Solve hw10

--- seq.app.c
+++ seq.app.c
@@ -1,10 +1,34 @@
 #include "usyscall.h"
 
 
-int atoi(const char *str) {
+int atoi(const char* str) {
+    int res = 0;
+    for (int i = 0; str[i] != '\0'; i++) res = res * 10 + str[i] - '0';
+    return res;
 }
 
-int itoa(int v, char *d) {
+int itoa(int v, char* d) {
+    char buf[32];
+    char* cur = buf;
+
+    unsigned int sign = v < 0, uv;
+    if (sign) uv = -v;
+    else uv = v;
+
+    while (uv > 0 || cur == buf) {
+        *(cur++) = (char) (uv % 10 + '0');
+        uv /= 10;
+    }
+
+    unsigned int len = cur - buf;
+    if (sign) {
+        *(d++) = '-';
+        len++;
+    }
+
+    while (cur > buf) *(d++) = *(--cur);
+
+    return (int) len;
 }
 
 int main(int argc, char* argv[]) {
