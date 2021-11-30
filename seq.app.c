2a19fe6ac3899a29543ffe63ccc445b22929fc09
Alexander Fedkin <st087201@student.spbu.ru>
2021-12-01 02:33:45 +0300

    solve hw10

--- seq.app.c
+++ seq.app.c
@@ -2,9 +2,45 @@
 
 
 int atoi(const char *str) {
+    int v = 0;
+
+    int neg = 0;
+
+    if (*str == '-') {
+        str++;
+        neg = 1;
+    }
+
+    while ('0' <= *str && *str <= '9') {
+        v *= 10;
+        v += (*(str++) - '0');
+    }
+
+    return neg ? -v : v;
 }
 
 int itoa(int v, char *d) {
+    if (v < 0) {
+        v = -v;
+        *d = '-';
+        d++;
+    }
+
+    if (!v) {
+        *d = '0';
+        return 1;
+    }
+
+    int v_dummy = v;
+    int length = 0;
+
+    for (; v_dummy; v_dummy /= 10) length++;
+
+    for (int i = length - 1; i >= 0; i--) {
+        d[i] = '0' + (v % 10);
+        v /= 10;
+    }
+    return length;
 }
 
 int main(int argc, char* argv[]) {
